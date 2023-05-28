1d2c54ad5c723ef564c2b9dc5d9e0b345902b749
karasik132 <131471331+karasik132@users.noreply.github.com>
2023-05-28 07:31:48 +0200

    Update qw.php

--- qw.php
+++ qw.php
@@ -1,27 +1,19 @@
 <?php
-$name = $_POST['name'];
-$tel = $_POST['telephone'];
-
-$name = htmlspecialchars($name);
-$tel = htmlspecialchars($tel);
-
-$name = urlencode($name);
-$tel = urlencode($tel);
-
-$name = trim($name);
-$tel = trim($tel);
-
-if (mail("to_mail@mail.ru",
-     "Pest Reject",
-     "Имя: ".$name."\n".
-     "Телефон ".$tel,
-     "From: script@mail.ru \r\n")
-){
-     header("Location: /thank_you.html");
-}
-
-else {
-     echo ("Error");
-}
-
+/* Здесь проверяется существование переменных */
+if (isset($_POST['name'])) {$phone = $_POST['name'];}
+if (isset($_POST['phone'])) {$name = $_POST['phone'];}
+ 
+/* Сюда впишите свою эл. почту */
+$myaddres  = "email@yandex .ru"; // кому отправляем
+ 
+/* А здесь прописывается текст сообщения, \n - перенос строки */
+$mes = "Тема: Заказ обратного звонка!\nТелефон: $phone\nИмя: $name";
+ 
+/* А эта функция как раз занимается отправкой письма на указанный вами email */
+$sub='Заказ'; //сабж
+$email='Заказ обратного звонка'; // от кого
+$send = mail ($myaddres,$sub,$mes,"Content-type:text/plain; charset = utf-8\r\nFrom:$email");
+ 
+ini_set('short_open_tag', 'On');
+header('Refresh: 3; URL=index.html');
 ?>
